486077aac1bf4e9482f5f70686954fce2474182a
ddangme <ssilver121@naver.com>
2024-01-23 14:19:18 +0900

    feat: 조인 On절 - 조인 대상 필터링 코드

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -305,4 +305,25 @@ public class QuerydslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회 (teamB인 경우 Team 정보는 불러오지 않는다.)
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+     * SQL:  SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID = t.id AND t.name = 'teamA'
+     */
+    @DisplayName("조인 On절 - 조인 대상 필터링")
+    @Test
+    void join_on_filtering() {
+        // Given
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        // Then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 }
