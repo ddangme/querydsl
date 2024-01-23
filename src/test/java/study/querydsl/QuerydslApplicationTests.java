aa550834670f7b5acb353d88b3798556faaabacc
ddangme <ssilver121@naver.com>
2024-01-23 12:01:51 +0900

    test: 환경 설정 테스트 코드 변경

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -24,7 +24,7 @@ class QuerydslApplicationTests {
 		em.persist(hello);
 
 		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
 
 		Hello result = query
 				.selectFrom(qHello)
