4ce5dacaadcc536d0277156803fe511e661ec68a
ddangme <ssilver121@naver.com>
2024-04-21 23:16:49 +0900

    chore: QueryDSL 의존성 추가

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,37 +1,35 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
-import static org.assertj.core.api.Assertions.*;
-
+import javax.persistence.EntityManager;
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class QuerydslApplicationTests {
 
-	@Autowired EntityManager em;
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
 		Hello hello = new Hello();
 		em.persist(hello);
-
 		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello;
-
+		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
 		Hello result = query
 				.selectFrom(qHello)
 				.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
+		Assertions.assertThat(result).isEqualTo(hello);
+//lombok 동작 확인 (hello.getId()) Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
